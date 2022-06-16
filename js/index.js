22a0d23bc299247a783465e21ee5c23ba455df02
mxrain <mxrain@yeah.net>
2022-06-16 11:40:02 +0800

    add

--- js/index.js
+++ js/index.js
@@ -9,10 +9,10 @@ window.$docsify = {
     // coverpage: true, //封面页
     mergeNavbar: true, //小屏设备下合并导航栏到侧边栏。
     loadNavbar: true,//导航
-    name: '笔记',
+    name: '',
     maxLevel: 3,
     themeColor: '#39BAE8',
-    // repo: 'https://github.com/mxrain/mxrain.github.io',
+    repo: 'maox.ml',
     formatUpdated: '{MM}/{DD} {HH}:{mm}',//更新时间格式
 
     // 添加一键拷贝
@@ -33,4 +33,5 @@ window.$docsify = {
       crossChapter: true,
       crossChapterText: true,
     },
+    
   }
